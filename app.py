168da9666e9b0ae4f0b2b12bc087c8f53a519bf9
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-02-19 08:37:00 +0200

    tests

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-#starting a basic function
-
 import numpy as np
-
+#starting a basic function
 # sigmoid function
 def nonlin(x,deriv=False):
     if(deriv==True):
@@ -24,7 +22,7 @@ np.random.seed(1)
 # initialize weights randomly with mean 0
 syn0 = 2*np.random.random((3,1)) - 1
 
-for iter in range(10000):
+for iter in range(1000000):
 
     # forward propagation
     l0 = X
