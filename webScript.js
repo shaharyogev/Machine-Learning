7ea08ec3917b431c862cf3ee518990f5ebbf20dd
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-02-20 08:21:54 +0200

    click1

---
+++ webScript.js
@@ -0,0 +1,20 @@
+
+// Under date range choose from Feb 1st till now
+
+
+dateRangeSelector =(period) =>{
+	const dateButtonClass = document.getElementsByClassName("alert-info");
+	dateButtonClass[0].querySelector('input[type="text"]').click();
+	
+}
+
+dateRangeSelector();
+
+
+
+
+//Change “Days running” to 30
+
+
+
+//Under “Ad Type” choose “Popup”
