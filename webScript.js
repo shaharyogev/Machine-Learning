564d1ed22187a2cf71f5ff44c9462143cb26c109
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-02-21 08:36:13 +0200

    date range + days running

--- webScript.js
+++ webScript.js
@@ -3,23 +3,40 @@
 
 
 dateRangeSelector =(period) =>{
-	//const dateButtonClass = document.getElementsByClassName("alert-info");
-	const daterangepicker = document.getElementsByClassName("daterangepicker");
-	let liList = daterangepicker.querySelector("li[data-range-key="+period+"]");
-	liList
-	//Click the data renge 
-	//dateButtonClass[0].querySelector('input[type="text"]').click();
-	//apand class active - data-range-key='This Month'
-	
+	//select the ranges element
+	const ranges = document.getElementsByClassName("ranges");
+
+	//clear the current class
+	let clearClass = ranges[0].firstChild.querySelectorAll('li[data-range-key]');
+	clearClass.forEach(i => {
+		i.classList.remove("active");
+	});
+
+	//add the active class to the wanted range
+	let liList = ranges[0].firstChild.querySelector('li[data-range-key="'+period+'"]');
+	liList.classList.add("active");
+
+
+	return liList;
 }
 
-dateRangeSelector();
+dateRangeSelector('This Month');
 
 
 
 
-//Change “Days running” to 30
+//Change “Days running” to 30  
 
+daysRunningSelector =(period) =>{
+	//select the "days-to pull-right"
+	const daysTo = document.getElementsByClassName("days-to pull-right");
+
+	//insert the value
+	daysTo[0].innerText = period;
+
+	return daysTo;
+}
+daysRunningSelector(30);
 
 
 //Under “Ad Type” choose “Popup”
