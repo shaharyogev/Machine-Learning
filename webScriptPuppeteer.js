c6bb7fff7c5314f1a785bc756cfd96ab66e516b9
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-03-13 08:07:04 +0200

    start new web script with puppeteer

---
+++ webScriptPuppeteer.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+	const browser = await puppeteer.launch({headless: false}); // default is true
+	const page = await browser.newPage();
+	page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36')
+	await page.goto('');
+	await page.screenshot({path: 'example.png'});
+	
+	// start the page script
+	await page.evaluate(() => {
+		document.querySelector('button[type=submit]').click();
+	});
+
+  await browser.close();
+})();
