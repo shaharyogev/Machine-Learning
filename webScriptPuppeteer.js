d8e2ef9fd39f4b82e9ff4779055a7a1eaafb478c
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-03-13 08:28:15 +0200

    start new web script with puppeteer

--- webScriptPuppeteer.js
+++ webScriptPuppeteer.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 	const browser = await puppeteer.launch({headless: false}); // default is true
 	const page = await browser.newPage();
 	page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36')
-	await page.goto('');
-	await page.screenshot({path: 'example.png'});
+	await page.goto('https://www.instagram.com/');
+	//await page.screenshot({path: 'example.png'});
 	
 	// start the page script
 	await page.evaluate(() => {
